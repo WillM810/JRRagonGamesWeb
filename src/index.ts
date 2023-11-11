8307f9785c96d688e4ea0c1fc24e971c37793b9a
Will McVay <mcvay.will@gmail.com>
2023-11-11 03:02:07 -0500

    being serious

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ console.log('you have a fax');
 import express from 'express';
 
 const app = express();
-app.get('/', (req, res) => res.send('fuck'));
+app.get('/', (req, res) => res.send('no this is a real server now be serious.'));
 
 app.listen(80, () => console.log('yeah'));
