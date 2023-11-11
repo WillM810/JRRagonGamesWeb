810587cb9e4a5f26e85c8a149e90fb9fdd9026f9
Will McVay <mcvay.will@gmail.com>
2023-11-11 02:45:33 -0500

    server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 console.log('hello');
-console.log('you have a fax')
+console.log('you have a fax');
+
+import express from 'express';
+
+const app = express();
+app.get('/', (req, res) => res.send('fuck'));
+
+app.listen(80, () => console.log('yeah'));
