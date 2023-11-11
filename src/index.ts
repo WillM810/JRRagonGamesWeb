833d6aedb772189d2428ba082c966ef09f2cd460
Will McVay <mcvay.will@gmail.com>
2023-11-11 03:57:11 -0500

    static!

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ console.log('you have a fax');
 
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 
 const app = express();
-app.get('/', (req, res) => res.send('no this is a real server now be serious.'));
 
-console.log(path.join(__dirname, '../HorribleGame'));
-app.get('/HorribleGame', express.static(path.join(__dirname, '../HorribleGame')));
+console.log(path.join(__dirname, '../../..HorribleGame'));
+console.log(fs.readdirSync(path.join(__dirname, '../../../HorribleGame')));
+app.get('/HorribleGame', express.static(path.join(__dirname, '../../../HorribleGame')));
+
+
+
+app.get('/', (req, res) => res.send('no this is a real server now be serious.'));
 
 app.listen(8085, () => console.log('yeah'));
