a2cb760b5469302b6646c92c8151f1677dae2cab
Will McVay <mcvay.will@gmail.com>
2023-11-11 03:42:04 -0500

    add game ref

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ console.log('hello');
 console.log('you have a fax');
 
 import express from 'express';
+import path from 'path';
 
 const app = express();
 app.get('/', (req, res) => res.send('no this is a real server now be serious.'));
+
+app.get('/HorribleGame', express.static(path.join(__dirname, '../HorribleGame')));
 
 app.listen(8085, () => console.log('yeah'));
