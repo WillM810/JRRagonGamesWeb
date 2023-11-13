d31d0e4fcc3409a522a6cb14cc1f2d5e40e0fc3d
Will McVay <mcvay.will@gmail.com>
2023-11-13 05:22:58 -0500

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-console.log('hello');
-console.log('you have a fax');
-
 import express from 'express';
 import path from 'path';
-import fs from 'fs';
+
+const IS_DEV = process.env.NODE_ENV === 'development';
+console.log(process.cwd());
 
 const app = express();
 
-console.log(path.join(__dirname, '../../..HorribleGame'));
-console.log(fs.readdirSync(path.join(__dirname, '../../../HorribleGame')));
 app.use('/HorribleGame', express.static(path.join(__dirname, '../../../HorribleGame')));
+app.use('/static/:path', (req, res) => res.send(path.join(process.cwd(), `../${req.params.path}`)));
 
 
 
 app.get('/', (req, res) => res.send('no this is a real server now be serious.'));
 
-app.listen(8085, () => console.log('yeah'));
+app.listen(8085, () => console.log('Server Running.'));
